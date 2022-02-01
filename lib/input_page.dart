093b15dac8dacdbb182440695cea748260ff25a3
Kumar Kaustubh <kumarkaustubh20@gmail.com>
2022-02-01 17:26:17 +0530

    Reusable cards

---
+++ lib/input_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('BMI CALCULATOR')),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(child: ReusableCard(colour: Color(0xff1d1e33))),
+                Expanded(child: ReusableCard(colour: Color(0xff1d1e33))),
+              ],
+            )),
+            Expanded(child: ReusableCard(colour: Color(0xff1d1e33))),
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(child: ReusableCard(colour: Color(0xff1d1e33))),
+                Expanded(child: ReusableCard(colour: Color(0xff1d1e33))),
+              ],
+            )),
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+
+  ReusableCard({@required this.colour});
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'pallete.dart';
+import 'input_page.dart';
+
 
 void main() => runApp(BMICalculator());
 
@@ -6,31 +9,18 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // theme: ThemeData.dark(),
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Palette.pcolor,
+          appBarTheme: AppBarTheme(
+            backgroundColor: Palette.pcolor,
+          ),
+          scaffoldBackgroundColor: Palette.pcolor,
+          textTheme: TextTheme(
+            bodyText1: TextStyle(color: Color(0xffffffff)),
+          )),
       home: InputPage(),
     );
   }
 }
 
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
 
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
